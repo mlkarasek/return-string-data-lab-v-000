9467ea07e1fea1b79b5a8563e6ae4c1c896ea506
Monica  Karasek <mlkarasek@gmail.com>
2018-10-14 00:17:44 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,23 +3,23 @@ class ProductsController < ApplicationController
   def index
        @products = Product.all
    end
-   
+
     def create
        @product = Product.create(post_params)
        @product.save
        redirect_to products_path
    end
  end 
-   
+
     def new
        @product = Product.new
    end
-   
+
     def description
        product = Product.find(params[:id])
        render plain: product.description
    end
-   
+
     def inventory
        product = Product.find(params[:id])
        render plain: product.inventory > 0 ? true : false
